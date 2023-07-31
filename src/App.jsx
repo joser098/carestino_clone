5015d8fa854740916f79aae604b0ac9ea6e70f8b
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-07-31 15:59:30 -0300

    Feat: Boxes component made

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import Carrousel from "./components/Carrousel/Carrousel"
-import Header from "./components/Header/Header"
+import Carrousel from "./components/Carrousel/Carrousel";
+import styles from './App.module.css'
+import Header from "./components/Header/Header";
+import slider1 from "../src/assets/slider1.webp";
+import slider2 from "../src/assets/slider2.webp";
+import slider3 from "../src/assets/slider3.webp";
+import Box from "./components/Box/Box";
+import card from './assets/card.svg';
 
 function App() {
-
   return (
     <>
-    <Header/>
-    <Carrousel/>
-    <h1>Hola mundo</h1>
+      <Header />
+
+      <section>
+        <Carrousel slider1={slider1} slider2={slider2} slider3={slider3} />
+      </section>
+
+      <section className={styles.boxes}>
+        <Box  link={"#"} svg={card} title={"12 Cuotas"} text={"Sin interes"} />
+        <Box  link={"#"}  svg={card} title={"12 Cuotas"} text={"Sin interes"} />
+        <Box  link={"#"} svg={card} title={"12 Cuotas"} text={"Sin interes"} />
+        <Box  link={"#"} svg={card} title={"12 Cuotas"} text={"Sin interes"} />
+      </section>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
